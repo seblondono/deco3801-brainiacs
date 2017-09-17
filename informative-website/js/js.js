7b538e37bf62d7573a10b9971ed9982e6e41a93d
Sebastian Londono Mesa <sebastianlondonom@gmail.com>
2017-09-18 02:11:53 +1000

    - Added about section content. - Added angle down icon to let user know the page continues scrolling down. - Added home section to nav bar. - Added footer. - Added team name to the nav bar. - Added active section colour via JS. - Added name and last name input validation. Only letters can be entered in those fields. - Added changing image effect to intro section. Every 60 seconds the background image will change. - Added corresponding styles to the new HTML content.

--- informative-website/js/js.js
+++ informative-website/js/js.js
@@ -1,4 +1,15 @@
+const intro_background = document.querySelector('#intro');
 const form = document.querySelector("form");
+const home = document.querySelector('nav ul li:nth-child(1) a');
+const image_contest = document.querySelector('nav ul li:nth-child(2) a');
+const voting_system = document.querySelector('nav ul li:nth-child(3) a');
+const about = document.querySelector('nav ul li:nth-child(4) a');
+const contact = document.querySelector('nav ul li:nth-child(5) a');
+const navigation = [home, image_contest, voting_system, about, contact];
+const images = ['url("https://deco3801-brainiacs.uqcloud.net/images/The%20Visual%20Brain.jpg")',
+'url("https://deco3801-brainiacs.uqcloud.net/images/BrainSTORM.jpg")',
+'url("https://deco3801-brainiacs.uqcloud.net/images/Interconnectivity%20%E2%80%93%20Dance%20of%20SNAREs%20In%20The%20Dark.jpg")'];
+
 
 function formValidation(e) {
     e.preventDefault();
@@ -10,6 +21,7 @@ function formValidation(e) {
     const noError = '';
     const error = "1px solid red";
     const emailRegex = /\b[A-Z0-9a-z._%+-]+@[A-Z0-9a-z.-]+\.[A-Za-z]{2,4}\b/i;
+    const nameRegex = /^[a-zA-Z-'. ]+$/;
 
     for (var i = 0; i < e.target.elements.length - 1; i++) {
         var element = e.target.elements[i];
@@ -23,11 +35,15 @@ function formValidation(e) {
         emailAddress.style.border = error;
         textarea.style.border = error;
     } else {
-        if (!name.value) {
+        if (!name.value || !name.value.match(nameRegex)) {
             name.style.border = error;
+            name.value = '';
+            name.placeholder = 'Please enter a valid name. Only letters are allowed.';
         }
-        if (!lastName.value) {
+        if (!lastName.value || !name.value.match(nameRegex)) {
             lastName.style.border = error;
+            lastName.value = '';
+            lastName.placeholder = 'Please enter a valid last name. Only letters are allowed.';
         }
         if (!emailAddress.value || !emailAddress.value.match(emailRegex)) {
             emailAddress.style.border = error;
@@ -37,9 +53,33 @@ function formValidation(e) {
         if (!textarea.value) {
             textarea.style.border = error;
         }
-
     }
 
 }
 
+function active(e) {
+    navigation.forEach(function(item) {
+        item.classList.remove('active');
+    }, this);
+    e.target.classList.add('active');
+}
+
+let random = 0;
+function getRandomImage() {
+    let random_new = Math.floor(Math.random() * images.length);
+    if (random_new == random) {
+        getRandomImage();
+    } else {
+        intro_background.style.backgroundImage = images[random_new];
+        random = random_new;
+    }
+}
+
+const changeImage = setInterval(getRandomImage, 60000);
+
 form.addEventListener("submit", (e) => formValidation(e));
+home.addEventListener('click', (e) => active(e));
+image_contest.addEventListener('click', (e) => active(e));
+voting_system.addEventListener('click', (e) => active(e));
+about.addEventListener('click', (e) => active(e));
+contact.addEventListener('click', (e) => active(e));
