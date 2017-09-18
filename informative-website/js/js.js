05c65d87c30aebf950949f418bac2864791fab80
Sebastian Londono Mesa <sebastianlondonom@gmail.com>
2017-09-18 12:33:18 +1000

    - Added active class be default to home item in nav bar. - Now purchase button links to contact section. - Added dynamic active class to nav bar based on user window scrolling. - Now down navigation arrows go to corresponding sections. - Added animation to down navigation arrows.

--- informative-website/js/js.js
+++ informative-website/js/js.js
@@ -1,15 +1,113 @@
-const intro_background = document.querySelector('#intro');
-const form = document.querySelector("form");
-const home = document.querySelector('nav ul li:nth-child(1) a');
-const image_contest = document.querySelector('nav ul li:nth-child(2) a');
-const voting_system = document.querySelector('nav ul li:nth-child(3) a');
-const about = document.querySelector('nav ul li:nth-child(4) a');
-const contact = document.querySelector('nav ul li:nth-child(5) a');
-const navigation = [home, image_contest, voting_system, about, contact];
+// Intro background
+const intro = document.querySelector('#intro');
 const images = ['url("https://deco3801-brainiacs.uqcloud.net/images/The%20Visual%20Brain.jpg")',
 'url("https://deco3801-brainiacs.uqcloud.net/images/BrainSTORM.jpg")',
 'url("https://deco3801-brainiacs.uqcloud.net/images/Interconnectivity%20%E2%80%93%20Dance%20of%20SNAREs%20In%20The%20Dark.jpg")'];
 
+let random = 0;
+function getRandomImage() {
+    let random_new = Math.floor(Math.random() * images.length);
+    if (random_new == random) {
+        getRandomImage();
+    } else {
+        intro.style.backgroundImage = images[random_new];
+        random = random_new;
+    }
+}
+
+const changeImage = setInterval(getRandomImage, 60000);
+
+// Navigation Bar
+const home_navbar = document.querySelector('nav ul li:nth-child(1) a');
+const image_contest_navbar = document.querySelector('nav ul li:nth-child(2) a');
+const voting_system_navbar = document.querySelector('nav ul li:nth-child(3) a');
+const about_navbar = document.querySelector('nav ul li:nth-child(4) a');
+const contact_navbar = document.querySelector('nav ul li:nth-child(5) a');
+const navigation = [home_navbar, image_contest_navbar, voting_system_navbar, about_navbar, contact_navbar];
+
+function active(e) {
+    navigation.forEach(function(item) {
+        item.classList.remove('active');
+    }, this);
+    e.target.classList.add('active');
+}
+
+home_navbar.addEventListener('click', (e) => active(e));
+image_contest_navbar.addEventListener('click', (e) => active(e));
+voting_system_navbar.addEventListener('click', (e) => active(e));
+about_navbar.addEventListener('click', (e) => active(e));
+contact_navbar.addEventListener('click', (e) => active(e));
+
+// Navigation Arrows
+const intro_arrow = document.querySelector('.fa.fa-angle-down.fa-4x.intro');
+const image_contest_arrow = document.querySelector('.fa.fa-angle-down.fa-4x.image-contest');
+const voting_system_arrow = document.querySelector('.fa.fa-angle-down.fa-4x.voting-system');
+const about_arrow = document.querySelector('.fa.fa-angle-down.fa-4x.about');
+
+intro_arrow.addEventListener('click', () => {document.querySelector('#image-contest').scrollIntoView();});
+image_contest_arrow.addEventListener('click', () => {document.querySelector('#voting-system').scrollIntoView();});
+voting_system_arrow.addEventListener('click', () => {document.querySelector('#about').scrollIntoView();});
+about_arrow.addEventListener('click', () => {document.querySelector('#contact').scrollIntoView();});
+
+// Navigation as elements get in focus
+const sections = document.querySelectorAll('section');
+
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
+  
+function checkSection(e){
+    sections.forEach(section => {
+        // Half of the section is when transition happens
+        const slideInAt = (window.scrollY + window.innerHeight) - section.clientHeight / 2;
+        // Bottom of section for when scrolling from bottom to top of page
+        const sectionBottom = section.offsetTop + section.clientHeight;
+        const isHalfShown = slideInAt > section.offsetTop;
+        const isNotScrolledPast = window.scrollY < sectionBottom;
+    
+        if (isHalfShown && isNotScrolledPast) {
+            if (section.id == 'intro') {
+                home_navbar.classList.add('active');
+            } else if (section.id == 'image-contest') {
+                image_contest_navbar.classList.add('active');
+            } else if (section.id == 'voting-system') {
+                voting_system_navbar.classList.add('active');
+            } else if (section.id == 'about') {
+                about_navbar.classList.add('active');
+            } else if (section.id == 'contact') {
+                contact_navbar.classList.add('active');
+            }
+        } else {
+            if (section.id == 'intro') {
+                home_navbar.classList.remove('active');
+            } else if (section.id == 'image-contest') {
+                image_contest_navbar.classList.remove('active');
+            } else if (section.id == 'voting-system') {
+                voting_system_navbar.classList.remove('active');
+            } else if (section.id == 'about') {
+                about_navbar.classList.remove('active');
+            } else if (section.id == 'contact') {
+                contact_navbar.classList.remove('active');
+            }
+        }
+    });
+}
+
+window.addEventListener('scroll', debounce(checkSection));
+
+//Form validation
+const form = document.querySelector("form");
 
 function formValidation(e) {
     e.preventDefault();
@@ -57,29 +155,11 @@ function formValidation(e) {
 
 }
 
-function active(e) {
-    navigation.forEach(function(item) {
-        item.classList.remove('active');
-    }, this);
-    e.target.classList.add('active');
-}
-
-let random = 0;
-function getRandomImage() {
-    let random_new = Math.floor(Math.random() * images.length);
-    if (random_new == random) {
-        getRandomImage();
-    } else {
-        intro_background.style.backgroundImage = images[random_new];
-        random = random_new;
-    }
-}
-
-const changeImage = setInterval(getRandomImage, 60000);
-
 form.addEventListener("submit", (e) => formValidation(e));
-home.addEventListener('click', (e) => active(e));
-image_contest.addEventListener('click', (e) => active(e));
-voting_system.addEventListener('click', (e) => active(e));
-about.addEventListener('click', (e) => active(e));
-contact.addEventListener('click', (e) => active(e));
+
+
+
+
+
+
+
