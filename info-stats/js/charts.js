4dcfb10b5d670592bcdf14457ddf20fe4b2eacf3
James Thompson <james.thompson275@hotmail.com>
2017-09-20 15:56:27 +1000

    Switched back to old way due to CORS

--- info-stats/js/charts.js
+++ info-stats/js/charts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
  $.jqplot.config.enablePlugins = true;	
-	$.get("/backend/data/visits", function(data, status){
+	$.get("https://deco3801-brainiacs.uqcloud.net/backend/data/visits", function(data, status){
 		if (status=="success"){
 			visits= JSON.parse(data);
 			graph(visits);	
@@ -34,7 +34,7 @@ console.log(visits)
  
 }
 
-$.get("/backend/data/comments",function(data, status){
+$.get("https://deco3801-brainiacs.uqcloud.net/backend/data/comments",function(data, status){
 	if (status=="success"){
 		comments=JSON.parse(data);
 		commentTable(comments);
