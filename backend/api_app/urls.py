667a8947d26063719ff9ecaefdc4ff95807e2168
Unknown <james.thompson275@hotmail.com>
2017-10-25 21:36:24 +1000

    Ready to start working with AJAX

--- backend/api_app/urls.py
+++ backend/api_app/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^voter/new/', views.new_voter, name='new_voter'),
 ]
--- backend/api_app/views.py
+++ backend/api_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
+def new_voter():
+    if request.is_ajax():
+        message = "AJAX"
+    else:
+        message = "NOT AJAX"
+    return JsonResponse({'message':'From Server:'+message})
