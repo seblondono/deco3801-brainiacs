e68fd7f71e3412eb5676e47409a3c3c294fcfbe8
Unknown <james.thompson275@hotmail.com>
2017-10-25 20:25:10 +1000

    Tabs were there instead of spaces.

--- backend/api_app/models.py
+++ backend/api_app/models.py
@@ -3,15 +3,15 @@ from django.contrib.auth.hashers import make_password
 
 # Create your models here.
 class Voter(models.Model):
-	email = models.TextField(unique=True)
-	password = models.TextField()
-	fname = models.TextField(null=True, blank=True)
-	lname = models.TextField(null=True, blank=True)
-	postcode = models.TextField(null=True, blank=True)
+    email = models.TextField(unique=True)
+    password = models.TextField()
+    fname = models.TextField(null=True, blank=True)
+    lname = models.TextField(null=True, blank=True)
+    postcode = models.TextField(null=True, blank=True)
     
     def __str__(self):
         return str(self.email)
-"""    
+   
     def save(self, *args, **kwargs):
         self.password = make_password(self.password)
-        super(Model, self).save(*args, **kwargs)"""
+        super(Model, self).save(*args, **kwargs)
