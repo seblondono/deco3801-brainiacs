8777ea438c3923e26954b87e7438a834d8fe9425
James Thompson <james.thompson2@uqconnect.edu.au>
2017-10-25 10:21:58 +0000

    Something is broken, need to get it off server to check

--- backend/api_app/models.py
+++ backend/api_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 # Create your models here.
 class Voter(models.Model):
@@ -7,6 +8,10 @@ class Voter(models.Model):
 	fname = models.TextField(null=True, blank=True)
 	lname = models.TextField(null=True, blank=True)
 	postcode = models.TextField(null=True, blank=True)
-
-	def __str__(self):
-		return str(self.email)
+    
+    def __str__(self):
+        return str(self.email)
+"""    
+    def save(self, *args, **kwargs):
+        self.password = make_password(self.password)
+        super(Model, self).save(*args, **kwargs)"""
