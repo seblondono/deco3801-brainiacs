6d546ce0d1c24fa7b6ae5686d271cd4818321c79
Unknown <james.thompson275@hotmail.com>
2017-10-26 20:54:16 +1000

    Added a voted flag to model.

--- backend/api_app/models.py
+++ backend/api_app/models.py
@@ -8,11 +8,13 @@ class Voter(models.Model):
     fname = models.CharField(max_length=50, null=True, blank=True)
     lname = models.CharField(max_length=50, null=True, blank=True)
     postcode = models.IntegerField(null=True, blank=True)
+    voted = BooleanField(default=False)
     
     def __str__(self):
         return str(self.email)
    
     def save(self, *args, **kwargs):
+        #check password isn't already hashed
         if "argon2$argon2" not in self.password:
             self.password = make_password(self.password)
         super(Voter, self).save(*args, **kwargs)
